0677839143c635b188a4ff4ce060bc54a13f0011
Dominik <89161020+dmkrtz@users.noreply.github.com>
2023-08-18 11:55:14 +0200

    added a "hasnosocials" check that returns "None :(" to the frontend

--- dmkrtz_um_show_socials.php
+++ dmkrtz_um_show_socials.php
@@ -11,9 +11,9 @@ function dmkrtz_um_show_socials($atts) {
         }
         return;
     }
-
+	
     // Fetch the UM user for the post author if on a single post or page
-    // and check if shortcode forces displaying socials of current user when including "force-user" attr
+	// and check if shortcode forces displaying socials of current user when including "force-user" attr
     if ((is_single() || is_page()) && !in_array("force-user", $atts)) {
         um_fetch_user(get_the_author_meta("ID"));
     }
@@ -25,14 +25,19 @@ function dmkrtz_um_show_socials($atts) {
 
     $style = isset($atts['style']) ? " style='{$atts['style']}'" : '';
     $output = "<div class='um-profile-connect um-member-connect'$style>";
-
+	
+	$hasnosocials = true;
     foreach ($social as $k => $arr) {
         if (!um_profile($k)) continue; // Skip non-existent social profiles
-
+		$hasnosocials = false;
+		
         $match = array_key_exists('match', $arr) ? (is_array($arr['match']) ? $arr['match'][0] : $arr['match']) : null;
-
+		
         $output .= "<a href='" . esc_url(um_filtered_social_link($k, $match)) . "' style='background: " . esc_attr($arr['color']) . "; text-decoration: none;' target='" . esc_attr($arr['url_target'] ?? "_blank") . "' class='um-tip-n' title='" . esc_attr($arr['title']) . "'><i class='" . esc_attr($arr['icon']) . "'></i></a>";
     }
+	
+	if($hasnosocials)
+		$output .= "None :(";
 
     $output .= '</div>';
     return __($output, "dmkrtz-um-show-socials");
